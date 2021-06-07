42e8aad6c50ed418943120152cf76f843be30e9b
jan214 <49987427+jan214@users.noreply.github.com>
2021-06-07 13:35:16 +0200

    Update collision.js
    added comments

--- collision.js
+++ collision.js
@@ -16,11 +16,14 @@ function Collision(){
 
 		for(var i = 0; i < p1.getNormals().length; i+=3){
 			for(var i2 = 0; i2 < p2.getPoints().length; i2+=3){
+				//project all points onto all normals(axis)
 				pointstoother = p1.getNormals()[i]*p2.getPoints()[i2]+p1.getNormals()[i+1]*p2.getPoints()[i2+1]+p1.getNormals()[i+2]*p2.getPoints()[i2+2];
 
 				if(p1axis[i/3] == null){
+					//if this is the first loop only fill the array with the values
 					p1axis[i/3] = new Array(pointstoother,pointstoother);
 				}else{
+					//else check the new values against the old values and keep the min max of those
 					if(p1axis[i/3][0] > pointstoother){
 						p1axis[i/3][0] = pointstoother;
 					}
@@ -31,22 +34,16 @@ function Collision(){
 			}
 		}
 
-		/*console.log(p1axis[1][0]);
-		console.log(p1axis[1][1]);
-		console.log(p1.getMin()[1]);
-		console.log(p1.getMax()[1]);*/
-
 		for(var i = 0; i < p1axis.length; i++){
+			//check if the points of the polygon1 lay inside the min max of the other polygon2
 			seperate = (p1axis[i][1] < p1.getMin()[i] || p1axis[i][0] > p1.getMax()[i]);
-			//console.log(seperate);
 			if(seperate){
 				break;
 			}
 		}
 
-		//console.log(" ");
-
 		if(!seperate){
+			//if all points of polygon1 lay inside polygon2 check the other way around, if points of polygon2 lay inside polygon1
 			p2axis = new Array(p2.getNormals().length/3);
 
 			for(var i = 0; i < p2.getNormals().length; i+=3){
@@ -69,17 +66,14 @@ function Collision(){
 			}
 
 			for(var i = 0; i < p2axis.length; i++){
+				//check again if all points in polygon2 lay inside min max of polygon1
 				seperate = (p2axis[i][1] < p2.getMin()[i] || p2axis[i][0] > p2.getMax()[i]);
-				//console.log(seperate);
 				if(seperate){
 					break;
 				}
 			}
 		}
-
-		//console.log(" ");
-		//console.log(" ");
-
+		//if all points lay inside seperate = false, else if there is a point found outside seperate = true
 		p1.setColliding(seperate);
 		p2.setColliding(seperate);
 	}
